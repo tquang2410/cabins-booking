4e84127058c5c475f7b6e073d586f1eb02262fe7
tquang2410 <justicepencil@gmail.com>
2025-06-04 15:01:27 +0700

    Build SignIn page

--- app/account/page.js
+++ app/account/page.js
@@ -1,10 +1,14 @@
+import {auth} from "@/app/_libs/auth";
+
 export const metadata = {
     title: "Guests Area",
 };
-export default function Page()
+export default async function Page()
 {
+    const session = await auth();
+    const firstName = session.user.name.split(" ").at(0);
   return <h2
       className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome Wang
+      Welcome, {firstName}
   </h2>
 }
