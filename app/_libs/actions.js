6198cc5fe4e64f196676be3e0bcc99b927d30800
tquang2410 <justicepencil@gmail.com>
2025-06-04 15:12:21 +0700

    Build signout button

--- app/_libs/actions.js
+++ app/_libs/actions.js
@@ -1,6 +1,9 @@
 "use server";
-import {signIn} from "./auth";
+import {signIn, signOut} from "./auth";
 
 export async function signInAction() {
     await signIn("google", {redirectTo: "/account"});
+}
+export async function signOutAction() {
+    await signOut({redirectTo: "/"});
 }
