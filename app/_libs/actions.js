e547029d00b8d8bd22297975d5404f4fbf6378ac
tquang2410 <justicepencil@gmail.com>
2025-06-05 23:32:08 +0700

    Delete booking

--- app/_libs/actions.js
+++ app/_libs/actions.js
@@ -33,3 +33,23 @@ export async function signInAction() {
 export async function signOutAction() {
     await signOut({redirectTo: "/"});
 }
+export async function deleteReservation(bookingId) {
+    const session = await auth();
+    if (!session) throw new Error("You must be logged in");
+    const guestBookings = await getBookings(session.user.guestId);
+    const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+    if(!guestBookingIds.includes(bookingId))
+        throw new Error("Not allowed to delete this booking");
+
+    const {  error } = await supabase
+        .from("bookings")
+        .delete()
+        .eq("id", bookingId);
+
+    if (error) {
+        console.error(error);
+        throw new Error("Booking could not be deleted");
+    }
+    revalidatePath("/account/reservations")
+}
