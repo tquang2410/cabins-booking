4380d82261458ab13bc403a936f7f34ac240e127
tquang2410 <justicepencil@gmail.com>
2025-06-05 15:50:45 +0700

    Displaying a Loading Indicator using useFormStatus hook

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import {updateGuest} from "@/app/_libs/actions";
+import {useFormStatus} from "react-dom";
 export default function UpdateProfileForm({guest,children}) {
    const [count, setCount] = useState(0);
    const {fullName, email, nationality, nationalID, countryFlag} = guest ;
@@ -51,11 +52,22 @@ export default function UpdateProfileForm({guest,children}) {
             </div>
 
             <div className="flex justify-end items-center gap-6">
-                <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-                    Update profile
-                </button>
+                <Button/>
             </div>
         </form>
+    );
+}
+function Button() {
+    const { pending, data, method, action } = useFormStatus();
+    return (
+        <button className="bg-accent-500 px-8 py-4
+        text-primary-800 font-semibold
+        hover:bg-accent-600 transition-all disabled:cursor-not-allowed
+        disabled:bg-gray-500 disabled:text-gray-300"
+                disabled={pending}
+        >
+            {pending ? "Update ..." : "Update profile"}
+        </button>
     )
 }
 
