9458115d6b150f00ad5efdd54369325e696d48c3
tquang2410 <justicepencil@gmail.com>
2025-06-05 14:03:05 +0700

    Update profile using server action

--- app/_libs/actions.js
+++ app/_libs/actions.js
@@ -1,6 +1,30 @@
 "use server";
-import {signIn, signOut} from "./auth";
+import {auth,signIn, signOut} from "./auth";
+import {supabase} from "@/app/_libs/supabase";
 
+export async function updateGuest(formData) {
+    const session = await auth();
+    if (!session)
+        throw new Error("You must be signed in to update your profile.");
+    const nationalID = formData.get("nationalID");
+    const [nationality, countryFlag] = formData.get("nationality").split("%");
+    const regex = /^[a-zA-Z0-9]{6,12}$/;
+
+    if (!regex.test(nationalID)) {
+        throw new Error("National ID must be 6-12 alphanumeric characters");
+    }
+    const updateDate = {nationality, countryFlag, nationalID};
+    console.log(updateDate)
+
+    const { data, error } = await supabase
+        .from('guests')
+        .update(updateDate)
+        .eq('id', session.user.guestId)
+
+    if (error) {
+        throw new Error('Guest could not be updated');
+    }
+}
 export async function signInAction() {
     await signIn("google", {redirectTo: "/account"});
 }
--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,13 +1,15 @@
 import UpdateProfileForm from '@/app/_components/UpdateProfileForm';
 import SelectCountry from "@/app/_components/SelectCountry";
+import {getGuest} from "@/app/_libs/data-service";
+import {auth} from "@/app/_libs/auth";
 
 export const metadata = {
     title: 'Update profile'
 };
+export default async function Page(){
+    const session = await auth();
+    const guest = await getGuest(session.user.email);
 
-export default function Page() {
-    // CHANGE
-    const nationality = "portugal";
     return (
         <div>
             <h2 className="font-semibold text-2xl text-accent-400 mb-4">
@@ -18,12 +20,12 @@ export default function Page() {
                 Providing the following information will make your check-in process
                 faster and smoother. See you soon!
             </p>
-            <UpdateProfileForm>
+            <UpdateProfileForm guest={guest}>
                 <SelectCountry
                     name="nationality"
                     id="nationality"
                     className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-                    defaultCountry={nationality}
+                    defaultCountry={guest.nationality}
                 />
             </UpdateProfileForm>
 
